747a5f567a820db6d98eac44ad4602a10d4bd648
danielsantos2310 <danieldecooo@gmail.com>
2022-04-28 22:53:40 +0300

    context API added

---
+++ src/components/TransactionList.js
@@ -0,0 +1,18 @@
+import React,{useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const TransactionList = () => {
+  const {transaction} = useContext(GlobalContext)
+
+  return (
+    <>
+    <h3>History</h3>
+      <ul className="list">
+        {transaction.map(transaction=>((s)}
+        <li className="minus">
+          Cash <span>-$400</span><button className="delete-btn">x</button>
+        </li> 
+      </ul>
+    </>
+  )
+}
